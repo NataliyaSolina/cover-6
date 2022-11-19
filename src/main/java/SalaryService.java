0a84b4c73cb54c9aa26f7f152e0e2b1867bad12a
Nataliya <nataliya.solina@bsc-ideas.com>
2022-11-19 18:50:23 +0300

    Initial commit

---
+++ src/main/java/SalaryService.java
@@ -0,0 +1,12 @@
+public class SalaryService {
+
+    public int calculateSalary(int sales) {
+        int percent = 5;
+        int salary = sales * percent / 100;
+        int salaryLimit = 50_000;
+        if (salary > salaryLimit) {
+            salary = salaryLimit;
+        }
+        return salary;
+    }
+}
